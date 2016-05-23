407bd40b9ea0850145c25a29020570a59cb30f5f
fordanns <ford.1441@gmail.com>
2016-05-23 12:45:44 +0200

    Practica 9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,3 +18,9 @@ exports.check = function(req, res, next) {
 	res.render('quizzes/result', { result: result, 
 								   answer: answer });
 };
+
+// GET /author
+exports.author = function(req, res, next) {
+
+	res.render('author', {autor: "Daniel Fortun Sanchez" });
+}
